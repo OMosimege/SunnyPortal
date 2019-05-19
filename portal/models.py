ada9d81ac79cc728ade63c5647ffb59bfc875c60
OMosimege <onalerona.mosimege@gmail.com>
2019-05-19 21:03:52 +0200

    added models to app

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Energy(models.Model):
+    kilowatts = models.CharField(max_length=200)
+    time = models.DateTimeField('date published')
